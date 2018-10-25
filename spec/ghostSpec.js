446b2ac8eaa103429288197acd6242ec2411fd4a
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2018-10-25 17:23:26 -0500

    added more tests to the ghostSpec.js file

---
+++ spec/ghostSpec.js
@@ -0,0 +1,21 @@
+describe('Ghosts', function() {
+
+  var ghosts, clock;
+  var timeBetweenSteps = 100;
+  var ghostSound = new Audio('Soundfiles/pacman_eatghost.wav')
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    ghosts =  new Ghosts(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(ghosts.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a sound for eating a ghost', function() {
+    expect(ghostSound).to.not.equal(undefined);
+  });
+
+  //try to check and see if the color is actually changing from red to blue.
+});
